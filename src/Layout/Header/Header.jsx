fcf3d94d561026a898e8060cefc52cff17b6566d
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-07-28 18:33:13 +0500

    logo add

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,13 +1,14 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.jpeg"
 import Nav from '../../components/Nav'
 
 function Header() {
   return (
     <header className="py-[10px] bg-white flex justify-around  ">
-      <div className="">
-        {/* <Link to="/">
-              <img src="" alt="" />logo
-              </Link> */}
+      <div className="w-[50px] rounded-[50%]">
+        <Link to="/">
+              <img src={logo} alt="logo" className="rounded-[50%]" />
+              </Link>
       </div>
 
       <Nav />
